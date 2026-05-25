1483c5eb5af93f7d047e1f6f6498eddd72cd3a65
Arianne Nantel <ariannenantel@Ariannes-MacBook-Air.local>
2026-05-25 15:55:11 -0400

    more datasets cleaned

--- Plotting using polars.py
+++ Plotting using polars.py
@@ -27,6 +27,7 @@ def two_yaxis_graph(df1, df1_xaxis, df1_yaxis, df1_label, df2, df2_xaxis, df2_ya
 
     # Reverse x-axis for paleoclimate convention
     ax1.invert_xaxis()
+    #ax1.invert_yaxis()
 
     # Right axis: CO2
     ax2 = ax1.twinx()
@@ -92,7 +93,7 @@ def main():
 
     ice_accumulation_original = pl.read_excel(
         source="aicc2012official.xls",
-        sheet_name="Vostok",
+        sheet_name="EDC",
         read_options={"header_row":1}
     )
 
@@ -102,32 +103,124 @@ def main():
     comment_prefix="#"
     )
 
-    ocean_temp_clean = ocean_temp_original.with_columns(pl.all().cast(pl.Float64)).select(pl.col("age_ka_spline").alias("Age") * 1000, pl.col("MOT_spline").alias("Variation in ocean temp"))
+    accumulation_EPICA_C_original = pl.read_excel(
+        source="Accum Rate EPICA C.xlsx",
+        read_options={"header_row":127}
+    )
 
-    #For grimmer dataset
-    co2_concentration_clean = co2_concentration_original.select(pl.col("Gasage (yr BP)").alias("Gas Age"), "CO2 (ppmv)").filter(pl.col("Gas Age").is_between(350.168,798569))
+    melt_rate_original = pl.read_excel(
+        source="pcmeltco_50yr.xlsx",
+        read_options={"header_row":17}
+    )
 
-    # For Epica Dome C 700 000 years dataset
-    #co2_concentration_clean = co2_concentration_original.select(pl.col("Gasage (yr BP)").alias("Age"), "CO2 (ppmv)").filter(pl.col("Age").is_between(1001,699622))
+    d18o_original = pl.read_excel(
+        source="lisiecki2005-d18o-stack-noaa.xlsx",
+        read_options={"header_row":126}
+    )
 
-    ocean_temp_v2_clean = ocean_temp_v2_original.select(pl.col("gas_age_ky").alias("Age") * 1000, "MOT_KrN2")
+    # Grimmer dataset
+    ocean_temp_clean = (
+        ocean_temp_original
+        .with_columns(pl.all().cast(pl.Float64))
+        .select(
+            (pl.col("age_ka_spline")*1000).alias("Age"),
+            pl.col("MOT_spline").alias("Variation in ocean temp"))
+        .filter(
+            ~pl.any_horizontal(pl.all() == -999) &
+            pl.col("Age").is_between(0, 10500))
+    )
 
-    icecore_data_co2_composite_clean = icecore_data_co2_composite_original.select(pl.col("AICC2012 gas age (a B1950)").alias("Gas Age"), "CO2 (ppmv)").filter(pl.col("Gas Age").is_between(3168.82, 798569))
+    # Antartica 2015 co2 set
+    co2_concentration_clean = (
+        co2_concentration_original
+        .select(
+            pl.col("Gasage (yr BP)").alias("Gas Age"),
+            "CO2 (ppmv)")
+        .filter(
+            pl.col("Gas Age").is_between(350.168,798569))
+    )
 
-    ice_accumulation_clean = ice_accumulation_original.select(pl.col("Ice age (a B1950)").alias("Ice Age"), pl.col("Gas age (a B1950)").alias("Gas Age"), pl.col("Accu. Rate (m ie)").alias("Accumulation Rate")).filter(pl.col("Gas Age").is_between(0,500000))
+    # EPICA DOM C Ocean temp dataset
+    ocean_temp_v2_clean = (
+        ocean_temp_v2_original
+        .select(
+            (pl.col("gas_age_ky")*1000).alias("Age"),
+            "MOT_KrN2")
+        .filter(
+            ~pl.any_horizontal(pl.all() == -999),
+            pl.col("Age").is_between(0, 10500))
+    )
+    
+    # aicc2012 ice core dataset
+    icecore_data_co2_composite_clean = (
+        icecore_data_co2_composite_original
+        .select(
+            pl.col("AICC2012 gas age (a B1950)").alias("Gas Age"),
+            "CO2 (ppmv)")
+        .filter(
+            pl.col("Gas Age").is_between(3168.82, 798569))
+    )
 
-    air_temep_clean = air_temp_original.select(pl.col("age_calkaBP").alias("Age") * 1000, pl.col("tempanom-pxy").alias("Anomalies from proxy data")).filter(pl.col("Anomalies from proxy data").is_between(-900,900))
+    # aicc2012 official dataset
+    ice_accumulation_clean = (
+        ice_accumulation_original
+        .select(
+            pl.col("Ice age (a B1950)").alias("Ice Age"),
+            pl.col("Gas age (a B1950)").alias("Gas Age"),
+            pl.col("Accu. Rate (m ie)").alias("Accumulation Rate"))
+        .filter(
+            pl.col("Ice Age").is_between(0,40000) & 
+           ~pl.any_horizontal(pl.all() == -1))
+    )
 
-    print(ocean_temp_clean.head())
-    print(co2_concentration_clean.head())
-    print(ocean_temp_v2_clean.head())
-    print(icecore_data_co2_composite_clean.head())
-    print(ice_accumulation_clean.head())
-    print(air_temep_clean.head())
+    # friedrich 2016 temp dataset
+    air_temp_clean = (
+        air_temp_original
+        .select(
+            (pl.col("age_calkaBP")*1000).alias("Age"),
+            pl.col("tempanom-pxy").alias("Anomalies from proxy data"))
+        .filter(
+            ~pl.any_horizontal(pl.all() == -999))
+    )
+
+    # Accum Rate EPICA C dataset
+    accumulation_EPICA_C_clean = (
+        accumulation_EPICA_C_original
+        .select(
+            pl.col("age_yrBP1950").alias("Age"),
+            pl.col("A_EDC").alias("Accumulation Rate"))
+    )
+
+    # pcmeltco_50yry dataset
+    melt_rate_clean = (
+        melt_rate_original
+        .select(
+            (1950 - pl.col("Year")).alias("Age"),
+            "Melt percentage")
+    )
+
+    # lisiecki 2005 dataset
+    d18o_clean = (
+        d18o_original
+        .select(
+            (pl.col("age_calkaBP")*1000).alias("Age"),
+            "d18O_benthic")
+        .filter(
+            pl.col("Age").is_between(3168.82, 798569))
+    )
+
+    #print(ocean_temp_clean.head())
+    #print(co2_concentration_clean.head())
+    #print(ocean_temp_v2_clean.head())
+    #print(icecore_data_co2_composite_clean.head())
+    #print(ice_accumulation_clean.head())
+    #print(air_temep_clean.head())
+    #print(accumulation_EPICA_C_clean.head())
+    #print(d18o_clean.head())
 
     #figure1 = plot_graph(icecore_data_co2_composite_clean, "Gas Age", "CO2 (ppmv)", "Gas age vs CO2 concentration")
     #figure2 = plot_graph(ice_accumulation_clean, "Ice Age", "Accumulation Rate", "Ice Age vs Accumulation Rate for Vostok Ice core")
-    figure3 = two_yaxis_graph(air_temep_clean, "Age", "Anomalies from proxy data", "Air Temp Anomalies", ocean_temp_v2_clean, "Age", "MOT_KrN2", "Water temp anomally", "Comparing Air and Ocean anomalies")
+    figure3 = two_yaxis_graph(melt_rate_clean, "Age", "Melt percentage", "melt percentage", ocean_temp_clean, "Age", "Variation in ocean temp", "Ocean Temp anomalies", "Comparing melt percentage to ocean temperature anomalies")
 
     plt.show()
 
